76344dc2787e4f4d983391e4b09b8cd1ad53751e
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-02-04 13:09:06 +0500

    added a js assignment chapter 6 to 9

---
+++ app.js
@@ -0,0 +1,286 @@
+//<---------------------------- Chapter no 05 ( math expression )----------------------------------------->
+
+//Question no 01
+
+// var num1 = 20; 
+// var prep = " and "
+// var num2 = 50;
+// var prepo = " is "
+// var total =  num1 + num2;
+// document.write( "Sum of " + num1  +prep + num2 +prepo + total  );
+
+//Question no 02
+
+// **Subtraction**
+
+// var total =  num1 - num2;
+// document.write( "</br>Subtraction of " + num1  +prep + num2 +prepo + total  );
+
+// **Multiplication**
+
+// var total =  num1 * num2;
+// document.write( "</br>Product of " + num1  +prep + num2 +prepo + total  );
+
+// **Division**
+
+// var total =  num1 / num2;
+// document.write( "</br>Division of " + num1  +prep + num2 +prepo + total  );
+
+// **Modulus**
+
+// var total =  num1 % num2;
+// document.write( "</br>Modulus of " + num1  +prep + num2 +prepo + total  );
+
+
+//Question no 03
+
+// var value;
+// document.write("Value of variable after declaration is "+value);
+// value = 8;
+// document.write("</br>Initial value is "+ value);
+// var valueInc = ++value;
+// document.write("</br>Value after Increment is "+valueInc);
+// var sum = valueInc + 7 ;
+// document.write("</br>Value after addition is "+sum);
+// var valueDec = --sum;
+// document.write("</br>Value after Decrement is " + valueDec);
+// var div = valueDec % 3 ;
+// document.write('</br>Output: "The remainder is : " '+div);
+
+
+// Question no 04
+
+// var costOfTicket = 600;
+// var totalCost = costOfTicket * 5;
+// document.write("Total Cost to buy 5 movie ticket is " +totalCost );
+// document.write("PKR");
+
+// Question no 05
+// var tableName = "18"
+// document.write("Table of "+tableName)
+// var ta = "18"
+// var aa = 1
+// var ab = 2
+// var ac = 3
+// var ad = 4
+// var ae = 5
+// var af = 6
+// var ag = 7
+// var ah = 8
+// var ai = 9
+// var aj = 10
+
+// document.write("</br>"+ta+"x"+aa+"="+(aa*ta))
+// document.write("</br>"+ta+"x"+ab+"="+(ab*ta))
+// document.write("</br>"+ta+"x"+ac+"="+(ac*ta))
+// document.write("</br>"+ta+"x"+ad+"="+(ad*ta))
+// document.write("</br>"+ta+"x"+ae+"="+(ae*ta))
+// document.write("</br>"+ta+"x"+af+"="+(af*ta))
+// document.write("</br>"+ta+"x"+ag+"="+(ag*ta))
+// document.write("</br>"+ta+"x"+ah+"="+(ah*ta))
+// document.write("</br>"+ta+"x"+ai+"="+(ai*ta))
+// document.write("</br>"+ta+"x"+aj+"="+(aj*ta))
+
+
+
+
+
+// Question no 06
+
+// var C = 19 ;
+// var fToC = (C*9/5)+32 ;
+// document.write( C+"C° is "+fToC )
+// document.write(" F°</br>")
+
+// var F = 70 ;
+// var cToF = (F-32)*5/9 ;
+// document.write( F+"F° is "+cToF )
+// document.write(" C°")
+
+// Question no 07
+
+// document.write("<h1>Shopping Cart</h1>")
+// var priceItem1 = 650;
+// document.write("Price Item of 1 is " + priceItem1);
+// var quantityItem1 = 5;
+// document.write("</br>Quantity of Item 1 is " + quantityItem1 );
+// var priceItem2 = 100;
+// document.write("</br>Price Item of 2 is " + priceItem2);
+// var quantityItem2 = 7;
+// document.write("</br>Quantity of Item 2 is " + quantityItem2);
+// var shippingCharges = 100;
+// document.write("</br>Shipping Charges is " + shippingCharges);
+
+
+// var calculation = (priceItem1*quantityItem1)+(priceItem2*quantityItem2);
+// document.write("</br></br></br>Total Cost of your Order "+ calculation)
+// document.write(" /-Rs")
+
+
+
+//Question no 08
+
+// document.write("<h1>MarksSheets</h1>")
+//  var marksObtained = 804 ;
+//  var marksTotal = 980 ;
+// var percentage = (marksObtained/marksTotal)*100;
+
+// document.write("Total Marks is: "+marksTotal);
+// document.write("</br>Marks Obtained is: "+marksObtained);
+// document.write("</br>Percentage is: "+percentage);
+// document.write("%");
+
+
+// Question no 09
+// document.write("<h1>CURRENCY IN PKR</h1>")
+// var noOfDollors = 10;
+// var valueOf1Dollor = 104.80; 
+// var noOfRiyals = 25;
+// var valueOf1Riyal = 28;
+// var conversion = (noOfDollors*valueOf1Dollor)+(noOfRiyals*valueOf1Riyal);
+// document.write("Total Currency in PKR:" +conversion);
+
+
+// Question no 10
+
+// var number;
+// number = 2;
+// var solution = (number + 5) * 10 / 2 ;
+//  console.log(solution);
+
+//Question no 11
+
+// document.write("<h1>AGE CALCULATOR</h1>");
+
+// var currentYear = 2023;
+// var birthYear = 2004;
+// var calc = currentYear-birthYear;
+// document.write("Current Year:"+currentYear)
+// document.write("</br>Birth Year:"+birthYear)
+// document.write("</br>Your Age is:"+calc)
+
+
+// Question no 12
+// document.write("<h1>The Geometrizer</h1>")
+// var r = 20;
+// document.write("Radius of a circle is: "+r)
+// var 𝝅 = 3.142;
+// document.write("</br>Circumference of circle is :"+ 2*𝝅*r)
+// document.write("</br>Area of circle is:"+𝝅*r*r);
+
+// Question no 13
+// document.write("<h1>The Lifetime Supply Calculator</h1>")
+// var snackName = "Lays"
+// var currentAge = 19;
+// var maxAge = 65;
+// var perDay = 3;
+// var need = (maxAge-currentAge)*perDay;
+// document.write("Favourite Snacks: "+snackName)
+// document.write("</br>Current Age: "+currentAge)
+// document.write("</br>Estimated Maximum Age: "+maxAge )
+// document.write("</br>Amount of Snack per Day: "+perDay)
+// document.write("</br>You wil need "+need   +snackName)
+// document.write(" to last you untill the rip old age of "+maxAge);
+
+
+
+
+//<-------------------------------------Chapter 06 to 09 ----------------------------------------------------->
+
+
+//Question no 01
+// document.write("Result:")
+// var a = 10;
+// document.write("</br>The value of a is:"+a);
+// document.write("</br>------------------------</br></br>");
+// var a = ++a;
+// document.write("</br>The value of ++a is: "+a)
+// document.write("</br>Now the value of a is:"+a)
+// var a = a++;
+// document.write("</br></br>The value of a++ is:"+a);
+// document.write("</br>Now the value of a is: "+a)
+// var a3 = --a2; 
+
+
+
+
+
+
+
+// Question no 02
+
+// var a = 2 , b = 1;
+// var result = --a - --b + ++b + b--;
+// --a;1
+// --a - --b = 1 - 0 = 1
+// --a - --b + ++b = 1 - 0 + 1 = 2
+// --a - --b + ++b + b-- = 1 - 0 + 1 + 1 = 3
+
+//Question no 03
+
+// var fullname= window.prompt("Enter Your Name")
+// var greet = "Hello, "
+// alert(greet + fullname)
+
+
+//Question no 05
+
+
+// document.write("Table of 5 ")
+// document.write("</br>5x1=05 ")
+// document.write("</br>5x2=10 ")
+// document.write("</br>5x3=15 ")
+// document.write("</br>5x4=20 ")
+// document.write("</br>5x5=25 ")
+// document.write("</br>5x6=30 ")
+// document.write("</br>5x7=35 ")
+// document.write("</br>5x8=40 ")
+// document.write("</br>5x9=45 ")
+// document.write("</br>5x10=50 ")
+
+// var tableNo = prompt("Enter table NO ")
+
+// var aa = 1
+// var ab = 2
+// var ac = 3
+// var ad = 4
+// var ae = 5
+// var af = 6
+// var ag = 7
+// var ah = 8
+// var ai = 9
+// var aj = 10
+
+// document.write("Table of "+tableNo)
+// document.write("</br>"+tableNo+"x"+aa+"="+(aa*tableNo))
+// document.write("</br>"+tableNo+"x"+ab+"="+(ab*tableNo))
+// document.write("</br>"+tableNo+"x"+ac+"="+(ac*tableNo))
+// document.write("</br>"+tableNo+"x"+ad+"="+(ad*tableNo))
+// document.write("</br>"+tableNo+"x"+ae+"="+(ae*tableNo))
+// document.write("</br>"+tableNo+"x"+af+"="+(af*tableNo))
+// document.write("</br>"+tableNo+"x"+ag+"="+(ag*tableNo))
+// document.write("</br>"+tableNo+"x"+ah+"="+(ah*tableNo))
+// document.write("</br>"+tableNo+"x"+ai+"="+(ai*tableNo))
+// document.write("</br>"+tableNo+"x"+aj+"="+(aj*tableNo))
+
+
+
+
+// Question no 06
+var subject1 = prompt("Enter the Name of Subject 1 ");
+var subject2 = prompt("Enter the Name of Subject 2 ");
+var subject3 = prompt("Enter the Name of Subject 3 ");
+var totalMarks = 100;
+var allTotal = totalMarks*3;
+var subject1obt = +prompt("Enter the Marks of "+subject1)
+var subject2obt = +prompt("Enter the Marks of "+subject2)
+var subject3obt = +prompt("Enter the Marks of "+subject3)
+var obtOfSubjects = +subject1obt + subject2obt + subject3obt;
+var grade = subject1obt/totalMarks*100;
+var allGrades = obtOfSubjects/allTotal*100
+document.write("<table><tr><th>Subjects</th><th>Total Marks</th><th>Obtained Marks</th><th>Percentage</th></tr>")
+document.write("<tr> <td>"+subject1+" </td> <td>" + totalMarks + "</td> <td>" + subject1obt + "</td> <td> " + grade+"%" + "</td> </tr>")
+document.write("<tr> <td>"+subject2+" </td> <td>" + totalMarks + "</td> <td>" + subject2obt + "</td> <td> " + grade+"%" + "</td> </tr>")
+document.write("<tr> <td>"+subject3+" </td> <td>" + totalMarks + "</td> <td>" + subject3obt + "</td> <td> " + grade+"%" + "</td> </tr>")
+document.write("<tr> <td> <b>Total</b>  </td><td><b>" + allTotal  + "</b></td> <td><b>"+ obtOfSubjects + "</b></td> <td><b> " + allGrades+"%" + "</b></td> </tr>")
+document.write("</table>")
